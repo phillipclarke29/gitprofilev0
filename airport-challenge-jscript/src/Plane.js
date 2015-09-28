0317ab0c44df125502f9c0371989d8968e59c19c
philip <philip.clarke@redhood.co.uk>
2015-09-28 18:21:48 +0100

    added tests for planes to be flying when created and not flying when landed - tests passed with new functions

---
+++ airport-challenge-jscript/src/Plane.js
@@ -0,0 +1,33 @@
+function Plane() {
+  this.isFlying = true;
+
+};
+
+Plane.prototype.land = function() {
+  this.isFlying = false;
+
+
+};
+
+
+
+// Player.prototype.play = function(song) {
+//   this.currentlyPlayingSong = song;
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.pause = function() {
+//   this.isPlaying = false;
+// };
+//
+// Player.prototype.resume = function() {
+//   if (this.isPlaying) {
+//     throw new Error("song is already playing");
+//   }
+//
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
+// };
